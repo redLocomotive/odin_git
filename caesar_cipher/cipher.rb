32fb587ec7cefdfcddf822068b16f0bea0a9578a
Red Locomotive <fahd777@protonmail.com>
2021-06-05 10:18:38 -0400

    use shift_num instead of hard coded '5' value

--- caesar_cipher/cipher.rb
+++ caesar_cipher/cipher.rb
@@ -1,13 +1,13 @@
 require 'pry-byebug'
 
 def caesar_cipher (string, shift_num)
-    string.split('').reduce('') do |acc, c|
+    string.each_char.reduce('') do |acc, c|
         v = c.ord
-        case v
-        when 97..122 # lowercase 
-            ord = v + 5 > 122 ? 'a'.ord - 1 + (v + 5) % 122 : v + 5
-        when 65..90  # uppercase
-            ord = v + 5 > 90 ? 'A'.ord - 1 + (v + 5) % 90 : v + 5
+        case c
+        when 'a'..'z' # lowercase 
+            ord = v + 5 > 122 ? 'a'.ord - 1 + (v + shift_num) % 122 : v + 5
+        when 'A'..'Z'  # uppercase
+            ord = v + 5 > 90 ? 'A'.ord - 1 + (v + shift_num) % 90 : v + 5
         else # non alphabetical character
             ord = v 
         end
@@ -18,3 +18,4 @@ end
 v = caesar_cipher("What a string!", 5) 
 p v
 p v == "Bmfy f xywnsl!"
+
