994bd7a3150507cfc2866037a527132111dd8041
Red Locomotive <fahd777@protonmail.com>
2021-06-05 10:20:26 -0400

    bugfix

--- caesar_cipher/cipher.rb
+++ caesar_cipher/cipher.rb
@@ -3,11 +3,12 @@ require 'pry-byebug'
 def caesar_cipher (string, shift_num)
     string.each_char.reduce('') do |acc, c|
         v = c.ord
+        shifted_ord = v + shift_num
         case c
         when 'a'..'z' # lowercase 
-            ord = v + 5 > 122 ? 'a'.ord - 1 + (v + shift_num) % 122 : v + 5
+            ord = shifted_ord > 122 ? 'a'.ord - 1 + shifted_ord % 122 : shifted_ord
         when 'A'..'Z'  # uppercase
-            ord = v + 5 > 90 ? 'A'.ord - 1 + (v + shift_num) % 90 : v + 5
+            ord = shifted_ord > 90 ? 'A'.ord - 1 + shifted_ord % 90 : shifted_ord
         else # non alphabetical character
             ord = v 
         end
